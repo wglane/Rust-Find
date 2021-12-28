73ac04a832e3f54c43ce7e15741e2ea0085dd958
Lane <wglane@william-nightingale.lan>
2021-12-28 16:29:50 -0500

    use structopt

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 mod myfile;
 
 use std::path::PathBuf;
+use structopt::StructOpt;
 
-
-const DIRS: [&str; 1] = ["./src"];
+// const DIRS: [&str; 1] = ["./src"];
 // const PATTERNS: [&str; 1] = ["*.rs"];
 
 const BUFSIZE: usize = 1000;
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rust find", about = "A simple find utility, written in Rust.")]
+struct Opt {
+    #[structopt(short = "d", long = "dirs", parse(from_os_str), help = "list of directories to search")]
+    dirs: Vec<PathBuf>,
+
+    #[structopt(short = "p", long = "patterns", help = "list of patterns to use")]
+    patterns: Vec<String>,
+
+    #[structopt(short = "s", long = "size", help = "match files above size <size> bytes")]
+    size: Option<usize>,
+}
+
 fn main() {
-    let mut dirs: Vec<PathBuf> = DIRS.iter().map(|d| d.into()).collect();
+    // let mut dirs: Vec<PathBuf> = DIRS.iter().map(|d| d.into()).collect();
+    let opt: Opt = StructOpt::from_args();
+    let mut dirs = opt.dirs.clone();
     let mut files: Vec<myfile::MyFile> = Vec::with_capacity(BUFSIZE);
 
     while !dirs.is_empty() {
